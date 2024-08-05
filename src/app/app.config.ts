9e35b571fc7b3d51e6671bc0e24ed00bab8288d9
Ahmet Yapicilar <a.yapicilar96@gmail.com>
2024-08-05 23:15:30 +0200

    failed load old game

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,7 +4,9 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration(), provideAnimationsAsync()]
+  providers: [provideRouter(routes), provideClientHydration(), provideAnimationsAsync(), provideFirebaseApp(() => initializeApp({"projectId":"ring-of-fire-49e19","appId":"1:463069897060:web:a8e5d26a38d98584c25a71","storageBucket":"ring-of-fire-49e19.appspot.com","apiKey":"AIzaSyBAcZ0jvW66htavDmOJr01ax4Ynn-B53Sg","authDomain":"ring-of-fire-49e19.firebaseapp.com","messagingSenderId":"463069897060"})), provideFirestore(() => getFirestore())]
 };
--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import { Game } from '../../models/game';
 import { PlayerComponent } from '../player/player.component';
 import { MatIconModule } from '@angular/material/icon';
@@ -7,6 +7,9 @@ import { MatDialog } from '@angular/material/dialog';
 import { DialogAddPlayerComponent } from '../dialog-add-player/dialog-add-player.component';
 import { GameInfoComponent } from "../game-info/game-info.component";
 import {MatCardModule} from '@angular/material/card';
+import { Firestore, addDoc, doc, collection, collectionData, onSnapshot } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-game',
@@ -19,17 +22,43 @@ export class GameComponent implements OnInit {
   game!: Game;
   pickCardAnimation = false;
   currentCard: string = '';
-  
-  constructor(public dialog: MatDialog){
+  firestore: Firestore = inject(Firestore);
+   items$;
+
+
+   constructor(private route: ActivatedRoute,public dialog: MatDialog) {
+
+    const aCollection = collection(this.firestore, 'games');
+    this.items$ = collectionData(aCollection);
+      this.items$.subscribe(game => {
+        console.log('game', game);
+      })
   }
 
-  ngOnInit(): void {
+  ngOnInit():void {
     this.newGame();
+    this.route.params.subscribe((param) => {
+      console.log(param);
+
+    })
+
+  }
+
+  getGamesRef(){
+    return collection(this.firestore, 'games');
+  }
+
+  getSingleGameRef(docId: string){
+    return doc(collection(this.firestore, 'games'), docId);
   }
 
   newGame(){
     this.game = new Game();
-    console.log(this.game);
+    // console.log(this.game);
+     addDoc(this.getGamesRef(), this.game.toJson());
+  }
+
+  ngOnDestroy(){
   }
 
   takeCard(){
